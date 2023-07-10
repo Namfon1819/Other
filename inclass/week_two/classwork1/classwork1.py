3f0534a4252e74c3287c20c2542ac9fb66363afe
Namfon <6410301005@cdti.ac.th>
2023-07-10 23:23:54 +0700

    new commit

--- inclass/week_two/classwork1/classwork1.py
+++ inclass/week_two/classwork1/classwork1.py
@@ -1,20 +1,29 @@
 import cv2
 import numpy as np
 
+# Load the image
+image = cv2.imread(r'C:\teacher\inclass\week_two\classwork1\12.jpg')  # Read the image
 
-image = cv2.imread(r'C:\teacher\inclass\week_two\123.jpg', cv2.IMREAD_GRAYSCALE) 
+# Define the block size and color
+block_size = 16  # Adjust this value according to your needs
+block_color = (0, 255, 0)  # Set the color of the blocks (in BGR format)
 
-start = (0, 200) #p1
-end = (400, 200) #p2
-line = (500, 500, 500)  #สีขาวหรือสีอะไรก็ได้แล้วแต่เรากำหนด
+# Iterate over each block
+for y in range(0, image.shape[0], block_size):
+    for x in range(0, image.shape[1], block_size):
+        # Get the current block
+        block = image[y:y+block_size, x:x+block_size]
 
-output_image = np.zeros_like(image) # ทุกอย่างนอกจากเส้นคือ 0
+        # Apply intensity transformation or histogram equalization on the block
+        transformed_block = cv2.equalizeHist(block)
 
-cv2.line(output_image, start, end, line, thickness=2)
+        # Draw a rectangle around the block with the specified color
+        cv2.rectangle(image, (x, y), (x+block_size, y+block_size), block_color, thickness=2)
 
-cv2.imshow("Output Image", output_image)
+        # Replace the original block with the transformed block
+        image[y:y+block_size, x:x+block_size] = transformed_block
+
+# Display the result
+cv2.imshow('Result', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-#หนูงงกับโจทย์ได้มาแบบนี้ค่าาา
